9e3f00d8ea2df53238b99cef22a251a71de982bc
Louis75 <zestian@gmail.com>
2023-09-07 22:15:29 +0900

    Update run_prerequites.ps1

--- run_prerequites.ps1
+++ run_prerequites.ps1
@@ -2,4 +2,4 @@ invoke-webrequest -Uri https://sca-downloads.s3.amazonaws.com/cli/2.4.2/ScaResol
 Expand-Archive -Path C:\ScaResolver-win64.zip -DestinationPath C:\
 echo "directory c:"
 dir c:\
-echo ${INPUT_WORKINGDIRECTORY}
+dir env:
